47b5601a348d9829bbd0467c7153a98b79696b1f
FitriYusa <muhammadfitz@gmail.com>
2024-08-05 12:51:33 +0800

    Application testing
    trying to get the application with data available in feature

--- Yuzee_sign_in_cucumber/cypress/e2e/spec.cy.feature
+++ Yuzee_sign_in_cucumber/cypress/e2e/spec.cy.feature
@@ -8,8 +8,22 @@ Feature: Yuzee Create Institution
       
       When the user initiate the sign in 
       And the user provides valid sign in details
+        | email                                      |
+        | abs1722237343963@vvocqwdp.mailosaur.net    |
       And the user submit the sign in form
       Then the user will be in homepage
-      When the user initiate the insitution
+      
+      Given the user initiate the insitution
       And the user provides valid information details
       When the user submit the application
+
+
+      Given the user initiate the education
+      When the user provides valid application details
+        | institute                           | course                                              | intake                       |
+        | Aust Link Pty Ltd                   | Certificate III Civil Construction Plant Operations | February 2024 - January 2025 |
+        | Aust Link Pty Ltd                   | Certificate III Civil Construction Plant Operations | July 2024 - July 2025        |
+        | Aust Link Pty Ltd                   | Certificate III Driving Operationsa                 | January 2024 - December 2024 |
+        | Australian National University      | Angular Js                                          | January 2025                 |
+        | Australian National University      | React Js                                            | January 2025                 |
+        | Australian National University      | React Js                                            |  June 2025                   |
--- Yuzee_sign_in_cucumber/cypress/e2e/step_definition/spec.js
+++ Yuzee_sign_in_cucumber/cypress/e2e/step_definition/spec.js
@@ -3,16 +3,21 @@ import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor"
 
 Given("the user is on Yuzee homepage is open", () => {
     cy.visit('/')
-    cy.intercept('POST', 'https://auth.yuzee.click/quick-blox/api/v1/quickblox/login').as('signinRequest');
+    cy.intercept('POST', 'https://auth.yuzee.click/quick-blox/api/v1/quickblox/login').as('signinRequest')
+    cy.intercept('GET', 'https://auth.yuzee.click/accessibility-service/api/v1/claim/association/entityId/a19781dd-643b-41a9-b8ab-b925f4e605fc/entityType/USER').as('userRequest')
 })
 
 When("the user initiate the sign in", () => {
     cy.contains('Sign in').click()
 })
 
-When("the user provides valid sign in details", () => {
-    cy.get('[placeholder="Email"]').clear().type('abs1722237343963@vvocqwdp.mailosaur.net')
-    cy.get('[type="password"]').clear().type('Admin@1234')
+When("the user provides valid sign in details", (dataTable) => {
+
+    dataTable.hashes().forEach(row => {
+        cy.get('[placeholder="Email"]').clear().type(row.email)
+        cy.get('[type="password"]').clear().type('Admin@1234')
+    })
+    
 })
 
 When("the user submit the sign in form", () => {
@@ -35,7 +40,7 @@ Then("the user will be in homepage", () => {
     // cy.url({ timeout: 10000 }).should('include', 'https://dev.yuzee.click/user-control-center/landing')
 })
 
-When("the user initiate the insitution", () => {
+Given("the user initiate the insitution", () => {
     cy.get('[id="dropdownPlace"]').click()
     cy.get('.dropdown-toggle > .btn').click()
     cy.get('.row > :nth-child(1) > .m-bottom > .btn-cont-block').click()
@@ -45,8 +50,9 @@ When("the user provides valid information details", () => {
 
     cy.intercept('POST', 'https://auth.yuzee.click/institute/api/v1/').as('createRequest')
 
-
-    cy.get('[id="instituteName"]').type('University of Bread')
+    const randomString = new Date().getTime();
+    
+    cy.get('[id="instituteName"]').type(`University of Bread ${randomString}`)
     cy.get('[id="tagline"]').type('Sungguh Enak dimakan begitu saja')
     cy.get('[id="description"]').type('Established in 1986, Gardenia Bakeries (KL) Sdn. Bhd. (Gardenia KL) is a joint venture between QAF Limited, a Singapore listed food group which also operates Gardenia Singapore and Gardenia Philippines, and Padiberas National Berhad of Malaysia. Our bakeries operate 24/7 to ensure that consumers enjoy fresh Gardenia products every day.')
     cy.get('[name="webSite"]').type('https://www.gardenia.com.my/about-us')
@@ -71,6 +77,7 @@ When("the user provides valid information details", () => {
     cy.get('[name="city_name"]').type('Kuala Lumpur')
     cy.get('[name="state_name"]').type('Kuala Lumpur')
     cy.get('[name="country_name"]').type('Malaysia')
+
     cy.get('[name="postal_code"]').clear().type('MY43200')
 
     cy.get('[role="combobox"]').eq(1).click()
@@ -90,15 +97,65 @@ When("the user provides valid information details", () => {
     cy.contains('7:30 PM').click()
 })
 
-
 When("the user submit the application", () => {
- 
 
     cy.get('.ng-star-inserted > .btn-blue').should('be.visible').click()
     
-    cy.wait('@createRequest', { timeout: 10000 }).then((interception) => {
+    // cy.wait('@createRequest', { timeout: 10000 }).then((interception) => {
+    //     expect(interception.response.statusCode).to.equal(200)
+    // })
+   
+}) 
+
+Given("the user initiate the education", () => {
+    cy.wait('@userRequest')
+    cy.intercept('GET','https://auth.yuzee.click/users/api/v1/users/user/id/a19781dd-643b-41a9-b8ab-b925f4e605fc').as('applicationRequest')
+
+    cy.visit('/user-control-center/landing')
+    cy.get(':nth-child(2) > .zoom-in-hover > img').click()
+    cy.contains('h2','Undergraduate').click()
+})
+
+When("the user provides valid application details", (dataTable) => {
+    
+    
+    cy.wait('@applicationRequest', { timeout : 10000}).then((interception) => {
         expect(interception.response.statusCode).to.equal(200)
     })
 
-   
- })  
+    cy.wait(3000)
+    cy.contains('Direct application').should('be.visible').click()
+    cy.wait(3000)
+    cy.contains('Create New').should('be.visible').click()
+
+    // for (let i = 0; i < 3; i++) {
+    //     cy.contains('Direct application').should('be.visible').click()
+    //     cy.contains('Create New').should('be.visible').click()
+    // }
+
+    cy.contains('Who is this application for?').should('be.visible')
+    cy.contains("Student at").should('be.visible').click()
+
+    cy.contains('Search institute').click({force : true})
+
+    dataTable.hashes().forEach(row => {
+        cy.contains(row.institute).click()
+       
+        cy.contains('Search Course').click({force : true})
+        cy.contains(row.course).click();
+ cy.get('.modal-footer > .btn-blue').click({force : true})
+        cy.contains('Select intake').click({ force: true });
+        cy.contains(row.intake).click({ force: true });
+        
+    })
+    
+    
+
+    // cy.contains('Select study mode').click({force : true})
+    // cy.contains('PART_TIME').click({force : true})
+    // cy.contains('Select delivery type').click({force : true})
+    // cy.contains('CLASSROOM').click({force : true})
+
+    // cy.contains('Next').eq(0).click()
+    // cy.contains('Next').eq(1).click()
+})
