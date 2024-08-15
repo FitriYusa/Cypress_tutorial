affe6ca3c418a12650934fd1727248b521a168b6
FitriYusa <muhammadfitz@gmail.com>
2024-08-15 17:06:04 +0800

    update sign in

--- Yuzee_work_availability/cypress/e2e/step_definitions/spec.js
+++ Yuzee_work_availability/cypress/e2e/step_definitions/spec.js
@@ -10,7 +10,36 @@ const serverID = "vvocqwdp";
 Given("the user is on Yuzee homepage is open", () => {
     cy.visit('/')
     cy.intercept('POST', 'https://auth.yuzee.click/users/api/v1/public/users/signup').as('signupRequest')
+    cy.intercept('POST', 'https://auth.yuzee.click/quick-blox/api/v1/quickblox/login').as('signinRequest')
 })
+
+When("the user initiate the sign in", () => {
+    cy.contains('Sign in').click()
+})
+
+When("the user provides valid sign in details", (dataTable) => {
+
+    dataTable.hashes().forEach(row => {
+        cy.get('[placeholder="Email"]').clear().type(row.email)
+        cy.get('[type="password"]').clear().type('Admin@1234')
+    })
+    
+})
+
+When("the user submit the sign in form", () => {
+    
+    cy.get('[type="submit"]').contains('Sign in').click()
+
+    cy.wait('@signinRequest', { timeout: 10000 }).then((interception) => {
+      
+        let statusCode = interception.response.statusCode;
+  
+        expect(statusCode).to.equal(200)
+
+    });
+
+})
+
 
 When("the new user initiate the account creation process", () => {
     cy.contains('Join Yuzee').click()
@@ -133,7 +162,7 @@ Then("the new user will be redirect to completeting the Onboarding", () => {
 })
 
 Then("the new user will be redirect to the user control center page", () => {
-    cy.url().should('include', '/user-control-center/landing')
+    cy.url({ timeout: 10000 }).should('include', '/user-control-center/landing')
 })
 
 When("the user initiate to Go to profile", () => {
